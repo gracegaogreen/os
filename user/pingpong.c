c32a4143f8bbf3f31edf8f711c54f973bb202f0d
gracegaogreen <15922274697@qq.com>
2024-07-17 00:28:40 +0800

    my solution for util lab exercese 1

--- user/pingpong.c
+++ user/pingpong.c
@@ -4,72 +4,72 @@
 
 int main(int argc, char* argv[])
 {
+    int p2c[2], c2p[2]; //two pipes: p2c for parent-to-child, c2p for child-to-parent
 
-    int fd[2];//file descriptors for the pipe, fd[0] for read, fd[1] for write
-    
-    //create pipe
-    if (pipe(fd) == -1) {
-        fprintf(2, "Error: pipe(fd) error.\n");
+    // create the pipes
+    if (pipe(p2c) == -1 || pipe(c2p) == -1) {
+        fprintf(2, "Error: pipe() error.\n");
         exit(1);
     }
     
-    //create child process
+    // fork the process to create a child
     int pid = fork();
     if (pid == -1) {
         fprintf(2, "Error: fork() error.\n");
         exit(1);
     }
     
-    //child process
+    // child process
     if (pid == 0) {
         char buffer[1];
-        close(fd[1]);//close the write end
-        //read data
-        if (read(fd[0], buffer, 1) == -1) {
+        close(p2c[1]); // close the write end of p2c
+        close(c2p[0]); // close the read end of c2p
+
+        // read data from parent
+        if (read(p2c[0], buffer, 1) == -1) {
             fprintf(2, "Error: read() error in child.\n");
             exit(1);
         }
-        close(fd[0]);//close the read end
-        printf("%d: received ping\n", getpid());
-        //create a new pipe to write
-        if (pipe(fd) == -1) {
-            fprintf(2, "Error: pipe(fd) error.\n");
-            exit(1);
-        }
-        //write data back
-        if (write(fd[1], buffer, 1) == -1) {
+        
+        close(p2c[0]); // close the read end of p2c
+        printf("%d: received ping\n", getpid());// print received message
+        
+        // write data back to parent
+        if (write(c2p[1], buffer, 1) == -1) {
             fprintf(2, "Error: write() error in child.\n");
             exit(1);
         }
-        close(fd[1]);
+        close(c2p[1]); // close the write end of c2p
+
+        exit(0);
     } 
-    
-    //parent process
+    // parent process
     else {
         char buffer[1];
-        buffer[0] = 'a';//prepare data to be sent
-        close(fd[0]);
-        //write data
-        if (write(fd[1], buffer, 1) == -1) {
+        buffer[0] = 'a'; // data to send
+
+        close(p2c[0]); // close the read end of p2c
+        close(c2p[1]); // close the write end of c2p
+
+        // 2rite data to child
+        if (write(p2c[1], buffer, 1) == -1) {
             fprintf(2, "Error: write() error in parent.\n");
             exit(1);
         }
-        close(fd[1]); 
-        wait(0);//wait for the child process to complete
-        // create a new pipe to read
-        if (pipe(fd) == -1) {
-            fprintf(2, "Error: pipe(fd) error.\n");
-            exit(1);
-        }
-        close(fd[1]);
-        //read data
-        if (read(fd[0], buffer, 1) == -1) {
+        close(p2c[1]); // close the write end of p2c
+        wait(0);// wait for the child process to complete
+
+        // read data from the child
+        if (read(c2p[0], buffer, 1) == -1) {
             fprintf(2, "Error: read() error in parent.\n");
             exit(1);
         }
-        close(fd[0]);
+        close(c2p[0]); // close the read end of c2p
+
+        // print received message
         printf("%d: received pong\n", getpid());
+
+        exit(0);
     }
-    exit(0);
 }
 
